763d5232c6f5c43eff14daf28062af56cab9a5a6
Yash Raj <yashraj.pratap@gmail.com>
2022-01-18 14:03:31 +0530

    v2

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 
 function Bio() {
   const navigate = useNavigate();
-
   const nextPage = () => {
     navigate("/upload");
   };
--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -11,6 +11,8 @@ function ProjectDetails() {
     navigate("/upload");
   };
 
+  const companies = ["c1", "c2", "c3"];
+
   const [inputData, setInputData] = useState("");
   const [items, setItems] = useState([]);
   const addItem = () => {
@@ -27,99 +29,51 @@ function ProjectDetails() {
     setItems(updateditems);
   };
 
-  const [inputData1, setInputData1] = useState("");
-  const [items1, setItems1] = useState([]);
-  const addItem1 = () => {
-    if (!inputData1) {
-    } else {
-      setItems1([...items1, inputData1]);
-      setInputData1("");
-    }
-  };
-  const deleteItem1 = (id) => {
-    const updateditems1 = items1.filter((elem, ind) => {
-      return ind !== id;
-    });
-    setItems1(updateditems1);
-  };
-
   return (
     <div>
       <div class="project">
         <h2>Enter your project details</h2>
         <div class="projects">
-          <div className="project-card">
-            <div className="company">
-              <div className="box"></div>
-              <span className="chead">Accenture Projects</span>
-              <button className="upicon">
-                <KeyboardArrowUp />
-              </button>
-            </div>
-            <div className="project-list">
-              {items.map((elem, ind) => {
-                return (
-                  <div className="list-item" key={ind}>
-                    <li>{elem}</li>
-                    <button
-                      className="closeIcon"
-                      onClick={() => deleteItem(ind)}
-                    >
-                      <Close fontSize="smaller" />
-                    </button>
-                  </div>
-                );
-              })}
-            </div>
-            <div className="add-div">
-              <input
-                className="add-input"
-                type="text"
-                placeholder="Enter Project Title"
-                value={inputData}
-                onChange={(e) => setInputData(e.target.value)}
-              />
-              <button className="add-new" onClick={addItem}>
-                ADD NEW
-              </button>
-            </div>
-          </div>
-          <div className="project-card">
-            <div className="company">
-              <div className="box"></div>
-              <span className="chead">Accenture Projects</span>
-              <button className="upicon">
-                <KeyboardArrowUp />
-              </button>
-            </div>
-            <div className="project-list">
-              {items1.map((elem, ind) => {
-                return (
-                  <div className="list-item" key={ind}>
-                    <li>{elem}</li>
-                    <button
-                      className="closeIcon"
-                      onClick={() => deleteItem1(ind)}
-                    >
-                      <Close fontSize="smaller" />
-                    </button>
-                  </div>
-                );
-              })}
-            </div>
-            <div className="add-div">
-              <input
-                className="add-input"
-                type="text"
-                placeholder="Enter Project Title"
-                value={inputData1}
-                onChange={(e) => setInputData1(e.target.value)}
-              />
-              <button className="add-new" onClick={addItem1}>
-                ADD NEW
-              </button>
-            </div>
-          </div>
+          {companies.map((item) => {
+            return (
+              <div className="project-card">
+                <div className="company">
+                  <div className="box"></div>
+                  <span className="chead">{item}</span>
+                  <button className="upicon">
+                    <KeyboardArrowUp />
+                  </button>
+                </div>
+                <div className="project-list">
+                  {items.map((elem, ind) => {
+                    return (
+                      <div className="list-item" key={ind}>
+                        <li>{elem}</li>
+                        <button
+                          className="closeIcon"
+                          onClick={() => deleteItem(ind)}
+                        >
+                          <Close fontSize="smaller" />
+                        </button>
+                      </div>
+                    );
+                  })}
+                </div>
+                <div className="add-div">
+                  <input
+                    className="add-input"
+                    type="text"
+                    placeholder="Enter Project Title"
+                    value={inputData}
+                    onChange={(e) => setInputData(e.target.value)}
+                  />
+                  <button className="add-new" onClick={addItem}>
+                    ADD NEW
+                  </button>
+                </div>
+              </div>
+            );
+          })}
 
           <div class="buttons btnp">
             <input
